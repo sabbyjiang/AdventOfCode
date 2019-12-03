ed3281b4c68ece0792940b420c11bd1331f72a55
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2019-12-03 01:07:42 -0500

    day 3 part II

--- 2019/day03/index.js
+++ 2019/day03/index.js
@@ -40,11 +40,54 @@ const getWireCoordinates = (wire) => {
 }
 
 const wireOneCoordinates = _.without(_.uniq(getWireCoordinates(wireOne).map((coord) => `${coord[0]},${coord[1]}`)), '0,0')
-console.log(wireOneCoordinates)
 const wireTwoCoordinates = _.uniq(getWireCoordinates(wireTwo).map((coord) => `${coord[0]},${coord[1]}`))
 
 const overlap = _.intersection(wireOneCoordinates, wireTwoCoordinates).sort()
 const overlapDistances = overlap.map((coordString) => Math.abs(Number(coordString.split(',')[0])) + Math.abs(Number(coordString.split(',')[1])))
-console.log(overlap);
 
-// console.log(overlapDistances.sort((a, b) => a-b)[0]);
+console.log(_.min(overlapDistances));
+
+const getWireDistance = (wire) => {
+  let distanceObj = {}
+  let currDistance = 0;
+  let currCoordinates = [0, 0]
+  wire.forEach((instruction) => {
+    let [currX, currY] = currCoordinates;
+    let moveDirection = instruction.match(/[RLUD]/)[0];
+    let moveUnits     = Number(instruction.match(/\d+/)[0]);
+    for(let i = 0; i < moveUnits ; i++){
+      switch(moveDirection){
+        case 'L':
+          currX--;
+          break;
+        case 'R':
+          currX++;
+          break;
+        case 'U':
+          currY++;
+          break;
+        case 'D':
+          currY--;
+          break;
+        default:
+          throw 'ERROR'
+      }
+      currDistance += 1;
+      distanceObj['' + currX + ',' + currY] = distanceObj['' + currX + ',' + currY] ? distanceObj['' + currX + ',' + currY] : currDistance;
+    }
+    currCoordinates = [currX, currY]
+  })
+  return distanceObj;
+}
+
+const wireOneDistance = getWireDistance(wireOne);
+const wireTwoDistance = getWireDistance(wireTwo);
+
+const wireOneDistanceAtIntersections = _.pick(wireOneDistance, overlap)
+const wireTwoDistanceAtIntersections = _.pick(wireTwoDistance, overlap)
+
+let totalDistances = _.reduce(wireOneDistanceAtIntersections, (combinedDistances, wireOneDist, coord) => {
+  return combinedDistances.concat(wireOneDist + wireTwoDistanceAtIntersections[coord])
+}, [])
+
+console.log(_.min(totalDistances))
