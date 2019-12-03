0cd152196cbba10e996bc5fddd8654a121377c74
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2019-12-02 22:56:09 -0500

    day 2 part I

---
+++ 2019/day02/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+
+let text = fs.readFileSync('./input.txt', 'utf8');
+const inputForPartOne = text.split(',').map((num) => Number(num));
+
+const OPERATION_CODES = {
+  HALT: 99,
+  ADD: 1,
+  MULTIPLY: 2
+}
+
+const intCalculator = (input) => {
+  let outputArray = JSON.parse(JSON.stringify(input));
+  outputArray[1] = 12;
+  outputArray[2] = 2;
+  let i = 0;
+  let halt = false;
+  while(!halt) {
+    let opCode      = outputArray[i];
+    let paramOneLoc = outputArray[i+1];
+    let paramTwoLoc = outputArray[i+2];
+    let changeLoc   = outputArray[i+3];
+
+    switch(opCode){
+      case OPERATION_CODES.HALT:
+        halt = true;
+        break;
+      case OPERATION_CODES.ADD:
+        outputArray[changeLoc] = outputArray[paramOneLoc] + outputArray[paramTwoLoc];
+        i += 4;
+        break;
+      case OPERATION_CODES.MULTIPLY:
+        outputArray[changeLoc] = outputArray[paramOneLoc] * outputArray[paramTwoLoc];
+        i += 4;
+        break;
+      default:
+        console.log('ERROR');
+        halt = true;
+        break;
+    }
+  }
+
+  return outputArray;
+}
+
+console.log(intCalculator(inputForPartOne)[0])
