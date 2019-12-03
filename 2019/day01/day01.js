6d0cd6b4ac2a1bd26d5bb162d6329ace46e5d1f6
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2019-12-02 22:41:44 -0500

    day 1 part II

--- 2019/day01/day01.js
+++ 2019/day01/day01.js
@@ -8,3 +8,17 @@ const fuelPerModule = (mass) => Math.floor( mass / 3 ) - 2;
 let fuelNecessary = inputArray.reduce((totalFuelRequirement, mass) => {
   return totalFuelRequirement + fuelPerModule(mass);
 }, 0)
+
+// Part II
+const fuelPerModuleRecurssive = (mass) => {
+  let moduleFuel = fuelPerModule(mass)
+  if ( moduleFuel <= 0 ) {
+    return 0;
+  } else {
+    return moduleFuel + fuelPerModuleRecurssive(moduleFuel)
+  }
+}
+
+let fuelNecessaryRecurrsive = inputArray.reduce((totalFuelRequirement, mass) => {
+  return totalFuelRequirement + fuelPerModuleRecurssive(mass);
+}, 0)
