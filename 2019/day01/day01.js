fc87b45917a2f46a91399ace4031dc80a307a174
Sabrina Jiang <jiang.sabrina.m@gmail.com>
2019-12-02 22:41:33 -0500

    day 1 part I

--- 2019/day01/day01.js
+++ 2019/day01/day01.js
@@ -2,3 +2,9 @@ const fs = require('fs');
 
 let text = fs.readFileSync('./input.txt', 'utf8');
 const inputArray = text.split('\n');
+
+const fuelPerModule = (mass) => Math.floor( mass / 3 ) - 2;
+// Part I
+let fuelNecessary = inputArray.reduce((totalFuelRequirement, mass) => {
+  return totalFuelRequirement + fuelPerModule(mass);
+}, 0)
